4015e490a0272cea7572f23ab9efcc9c2fdfed9b
Reese Marshall <reese.amarshall@gmail.com>
2022-12-13 04:15:55 -0500

    commit tree 01e4168760d952dd78f52c45614420710c1af6e9

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+let feet = 600
 function scuberGreetingForFeet(){
   // Write your code here!
+  if (feet => 400 )
+  return 'This one is on me!';
+  else if (feet < 400 )
+  return 'That will be twenty bucks.';
+  else if (feet => 2000)
+  return 'That will be twenty bucks.';
 }
-
 function ternaryCheckCity(){
   // Write your code here!
 }
